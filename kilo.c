aa99bedb53aabab94fa5078b461dce529d01c160
Joe Cortopassi <joe@joecortopassi.com>
2021-01-10 07:18:11 -0600

    draw out screen with tildes signifying rows

--- kilo.c
+++ kilo.c
@@ -17,6 +17,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -52,6 +55,25 @@ char editorReadKey() {
     return c;
 }
 
+/*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y=0; y<24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -59,6 +81,8 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }        
@@ -70,6 +94,7 @@ int main() {
     enableRawMode();
 
     while(1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
     return 0;
